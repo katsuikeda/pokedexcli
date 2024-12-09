743a2e69d82548904a62eb32dd0c035c442e38ae
katsuikeda <katsuyukiikeda142857@gmail.com>
2024-12-08 23:28:05 -0800

    Add inspect command to view details about a caught Pokemon

--- repl.go
+++ repl.go
@@ -88,6 +88,11 @@ func getCommands() map[string]cliCommand {
 			description: "Attempt to catch a pokemon and add it to your pokedex",
 			callback:    commandCatch,
 		},
+		"inspect": {
+			name:        "inspect {pokemon_name}",
+			description: "View details about a pokemon in your pokedex",
+			callback:    commandInspect,
+		},
 		"exit": {
 			name:        "exit",
 			description: "Exit the Pokedex",
